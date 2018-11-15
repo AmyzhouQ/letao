6e55dd60cb7e10b89ddc6c373239191526c6bd47
AmyzhouQ <634764014@qq.com>
2018-11-15 23:42:46 +0800

    主页面基本框架完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -9,3 +9,36 @@ $( document ).ajaxStop(function() {
     NProgress.done();
   },2000)
 });
+
+// 点击分类管理二级菜单显示隐藏
+$(function(){
+  $(".cate").click(function(){
+    $(this).next().stop().slideToggle();  
+  })
+});
+
+// 单击icon-left 侧边栏滑出
+$(function(){
+  $(".icon-left").click(function(){
+    $(".lt-aside").toggleClass("hidemenu");
+    $(".lt-main").toggleClass("hidemenu"); 
+    $(".lt-topbar").toggleClass("hidemenu"); 
+  })
+})
+
+// 模态框退出功能
+$(function(){
+  $(".btn-out").click(function(){
+    $.ajax({
+      type: "get",
+      url: "/employee/employeeLogout",
+      dataType: "json",
+      success: function(info){
+        console.log(info);
+        if(info.success){
+          location.href = "login.html";
+        }
+      }
+    })
+  })
+})
